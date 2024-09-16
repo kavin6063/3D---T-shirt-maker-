4b39a4b46b217cc429dedb03f015fa7dfdb1babb
Kavin <kavinkarthickeyan315@gmail.com>
2024-09-16 18:57:00 +0530

    added fullTexture bug

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -34,9 +34,28 @@ const Customizer = () => {
       case "filepicker":
         return <FilePicker file={file} setFile={setFile} readFile={readFile} />;
       case "AIPicker":
-        return <AIPicker />;
+        return (
+          <AIPicker
+            prompt={prompt}
+            setPrompt={setPrompt}
+            generatingImg={generatingImg}
+            handleSubmit={handleSubmit}
+          />
+        );
       default:
         return null;
+    }
+  };
+  const handleSubmit = async (type) => {
+    if (!prompt) return alert("Please enter a prompt");
+
+    try {
+      // call our backend to genrate an image
+    } catch (error) {
+      alert(error);
+    } finally {
+      setGeneratingImg(false);
+      setActiveEditorTab("");
     }
   };
   const handleDecals = (type, result) => {
@@ -56,6 +75,7 @@ const Customizer = () => {
         break;
       case "stylishShirt":
         state.isFullTexture = !activeFilterTab[tabName];
+        break;
       default:
         state.isFullTexture = false;
         state.isLogoTexture = true;
