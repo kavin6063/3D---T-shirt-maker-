24c1fb90ee22556c4d4aa213e77d898d55d135aa
Kavin <kavinkarthickeyan315@gmail.com>
2024-09-17 10:49:04 +0530

    added logo

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,11 @@ import { proxy } from "valtio";
 
 const state = proxy({
   intro: true,
-  color: "#EFBD48",
+  color: "#787272",
   isLogoTexture: true,
   isFullTexture: false,
-  logoDecal: "./threejs.png",
-  fullDecal: "./threejs.png",
+  logoDecal: "./logo-code.png",
+  fullDecal: "./logo-code.png",
 });
 
 export default state;
